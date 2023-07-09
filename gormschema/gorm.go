ffaa9ee78678980a79064059106fb377509dcfe8
Rotem Tamir <rotemtamir@gmail.com>
2023-07-09 11:39:34 +0300

    recordriver: use from sdk (#7)

--- gormschema/gorm.go
+++ gormschema/gorm.go
@@ -5,7 +5,7 @@ import (
 	"database/sql/driver"
 	"fmt"
 
-	"ariga.io/atlas-provider-gorm/recordriver"
+	"ariga.io/atlas-go-sdk/recordriver"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
