8670bd34cfbc42f647bacc85a6dc3d0abac9581b
dorav <dor.a2003@gmail.com>
2023-12-17 14:23:05 +0200

    fixed SDK pointer, added mysql test

--- gormschema/gorm_test.go
+++ gormschema/gorm_test.go
@@ -47,3 +47,26 @@ func TestPostgreSQLConfig(t *testing.T) {
 	require.Contains(t, sql, `CREATE UNIQUE INDEX IF NOT EXISTS "idx_locations_event_id"`)
 	require.NotContains(t, sql, "FOREIGN KEY")
 }
+
+func TestMySQLConfig(t *testing.T) {
+	l := New("mysql", WithConfig(&gorm.Config{}))
+	sql, err := l.Load(models.Location{}, models.Event{})
+	require.NoError(t, err)
+	require.Contains(t, sql, "CREATE TABLE `events`")
+	require.Contains(t, sql, "CREATE TABLE `locations`")
+	require.Contains(t, sql, "ALTER TABLE `events` ADD CONSTRAINT `fk_locations_event`")
+	require.Contains(t, sql, "ALTER TABLE `locations` ADD CONSTRAINT `fk_events_location`")
+	sess, ok := recordriver.Session("gorm")
+	require.True(t, ok)
+	sess.Statements = []string{}
+	l = New("mysql", WithConfig(
+		&gorm.Config{
+			DisableForeignKeyConstraintWhenMigrating: true,
+		},
+	))
+	sql, err = l.Load(models.Location{}, models.Event{})
+	require.NoError(t, err)
+	require.Contains(t, sql, "CREATE TABLE `events`")
+	require.Contains(t, sql, "CREATE TABLE `locations`")
+	require.NotContains(t, sql, "FOREIGN KEY")
+}
