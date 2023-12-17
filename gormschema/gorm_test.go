701fc9753c8776fe82c6a4899609b2efe204ee7d
dorav <dor.a2003@gmail.com>
2023-12-17 17:58:28 +0200

    fixed tests

--- gormschema/gorm_test.go
+++ gormschema/gorm_test.go
@@ -1,6 +1,7 @@
 package gormschema
 
 import (
+	"strings"
 	"testing"
 
 	"ariga.io/atlas-go-sdk/recordriver"
@@ -11,43 +12,44 @@ import (
 )
 
 func TestSQLiteConfig(t *testing.T) {
+	resetSession(t)
 	l := New("sqlite")
 	sql, err := l.Load(models.Pet{}, models.User{}, ckmodels.Event{}, ckmodels.Location{})
 	require.NoError(t, err)
-	require.Contains(t, sql, "CREATE TABLE `pets`")
-	require.Contains(t, sql, "CREATE TABLE `users`")
-	require.Contains(t, sql, "CREATE TABLE `events`")
-	require.Contains(t, sql, "CREATE TABLE `locations`")
-	require.Contains(t, sql, "FOREIGN KEY (`eventId`)")
-	require.Contains(t, sql, "FOREIGN KEY (`locationId`)")
-	require.Contains(t, sql, "CONSTRAINT `fk_users_pets` FOREIGN KEY")
+	stmts := strings.Split(sql, "\n")
+	require.Contains(t, stmts[0], "CREATE TABLE `users`")
+	require.Contains(t, stmts[2], "CREATE TABLE `pets`")
+	require.Contains(t, stmts[2], "CONSTRAINT `fk_users_pets` FOREIGN KEY (`user_id`)")
+	require.Contains(t, stmts[4], "CREATE TABLE `locations`")
+	require.Contains(t, stmts[4], "CONSTRAINT `fk_events_location` FOREIGN KEY (`eventId`)")
+	require.Contains(t, stmts[6], "CREATE TABLE `events`")
+	require.Contains(t, stmts[6], "CONSTRAINT `fk_locations_event` FOREIGN KEY (`locationId`)")
 	resetSession(t)
 	l = New("sqlite", WithConfig(&gorm.Config{
 		DisableForeignKeyConstraintWhenMigrating: true,
 	}))
 	sql, err = l.Load(models.Pet{}, models.User{})
 	require.NoError(t, err)
-	require.Contains(t, sql, "CREATE TABLE `pets`")
-	require.Contains(t, sql, "CREATE TABLE `users`")
-	require.NotContains(t, sql, "FOREIGN KEY")
+	stmts = strings.Split(sql, "\n")
+	require.Contains(t, stmts[0], "CREATE TABLE `users`")
+	require.Contains(t, stmts[2], "CREATE TABLE `pets`")
+	require.NotContains(t, stmts[2], "FOREIGN KEY")
 	resetSession(t)
 }
 
 func TestPostgreSQLConfig(t *testing.T) {
+	resetSession(t)
 	l := New("postgres")
 	sql, err := l.Load(ckmodels.Location{}, ckmodels.Event{}, models.User{}, models.Pet{})
 	require.NoError(t, err)
-	require.Contains(t, sql, `CREATE TABLE "users"`)
-	require.Contains(t, sql, `CREATE INDEX IF NOT EXISTS "idx_users_deleted_at"`)
-	require.Contains(t, sql, `CREATE TABLE "pets"`)
-	require.Contains(t, sql, `CREATE INDEX IF NOT EXISTS "idx_pets_deleted_at"`)
-	require.Contains(t, sql, `CONSTRAINT "fk_users_pets" FOREIGN KEY ("user_id")`)
-	require.Contains(t, sql, `CREATE TABLE "events"`)
-	require.Contains(t, sql, `CREATE UNIQUE INDEX IF NOT EXISTS "idx_events_location_id"`)
-	require.Contains(t, sql, `CREATE TABLE "locations"`)
-	require.Contains(t, sql, `CREATE UNIQUE INDEX IF NOT EXISTS "idx_locations_event_id"`)
-	require.Contains(t, sql, `ALTER TABLE "events" ADD CONSTRAINT "fk_locations_event" FOREIGN KEY ("locationId")`)
-	require.Contains(t, sql, `ALTER TABLE "locations" ADD CONSTRAINT "fk_events_location"`)
+	stmts := strings.Split(sql, "\n")
+	require.Contains(t, stmts[0], `CREATE TABLE "events"`)
+	require.Contains(t, stmts[2], `CREATE TABLE "locations"`)
+	require.Contains(t, stmts[4], `CREATE TABLE "users"`)
+	require.Contains(t, stmts[6], `CREATE TABLE "pets"`)
+	require.Contains(t, stmts[8], `ALTER TABLE "events" ADD CONSTRAINT "fk_locations_event" FOREIGN KEY ("locationId")`)
+	require.Contains(t, stmts[9], `ALTER TABLE "locations" ADD CONSTRAINT "fk_events_location"`)
+	require.Contains(t, stmts[10], `ALTER TABLE "pets" ADD CONSTRAINT "fk_users_pets" FOREIGN KEY ("user_id")`)
 	resetSession(t)
 	l = New("postgres", WithConfig(
 		&gorm.Config{
@@ -55,23 +57,25 @@ func TestPostgreSQLConfig(t *testing.T) {
 		}))
 	sql, err = l.Load(ckmodels.Location{}, ckmodels.Event{})
 	require.NoError(t, err)
-	require.Contains(t, sql, `CREATE TABLE "events"`)
-	require.Contains(t, sql, `CREATE TABLE "locations"`)
-	require.Contains(t, sql, `CREATE UNIQUE INDEX IF NOT EXISTS "idx_locations_event_id"`)
+	stmts = strings.Split(sql, "\n")
+	require.Contains(t, stmts[0], `CREATE TABLE "events"`)
+	require.Contains(t, stmts[2], `CREATE TABLE "locations"`)
 	require.NotContains(t, sql, "FOREIGN KEY")
 }
 
 func TestMySQLConfig(t *testing.T) {
+	resetSession(t)
 	l := New("mysql")
 	sql, err := l.Load(ckmodels.Location{}, ckmodels.Event{}, models.User{}, models.Pet{})
+	stmts := strings.Split(sql, "\n")
 	require.NoError(t, err)
-	require.Contains(t, sql, "CREATE TABLE `users`")
-	require.Contains(t, sql, "CREATE TABLE `pets`")
-	require.Contains(t, sql, "CONSTRAINT `fk_users_pets` FOREIGN KEY (`user_id`)")
-	require.Contains(t, sql, "CREATE TABLE `events`")
-	require.Contains(t, sql, "CREATE TABLE `locations`")
-	require.Contains(t, sql, "ALTER TABLE `events` ADD CONSTRAINT `fk_locations_event`")
-	require.Contains(t, sql, "ALTER TABLE `locations` ADD CONSTRAINT `fk_events_location`")
+	require.Contains(t, stmts[0], "CREATE TABLE `events`")
+	require.Contains(t, stmts[1], "CREATE TABLE `locations`")
+	require.Contains(t, stmts[2], "CREATE TABLE `users`")
+	require.Contains(t, stmts[3], "CREATE TABLE `pets`")
+	require.Contains(t, stmts[4], "ALTER TABLE `events` ADD CONSTRAINT `fk_locations_event`")
+	require.Contains(t, stmts[5], "ALTER TABLE `locations` ADD CONSTRAINT `fk_events_location`")
+	require.Contains(t, stmts[6], "ALTER TABLE `pets` ADD CONSTRAINT `fk_users_pets`")
 	resetSession(t)
 	l = New("mysql", WithConfig(
 		&gorm.Config{
@@ -80,13 +84,15 @@ func TestMySQLConfig(t *testing.T) {
 	))
 	sql, err = l.Load(ckmodels.Location{}, ckmodels.Event{})
 	require.NoError(t, err)
-	require.Contains(t, sql, "CREATE TABLE `events`")
-	require.Contains(t, sql, "CREATE TABLE `locations`")
+	stmts = strings.Split(sql, "\n")
+	require.Contains(t, stmts[0], "CREATE TABLE `events`")
+	require.Contains(t, stmts[1], "CREATE TABLE `locations`")
 	require.NotContains(t, sql, "FOREIGN KEY")
 }
 
 func resetSession(t *testing.T) {
 	sess, ok := recordriver.Session("gorm")
-	require.True(t, ok)
-	sess.Statements = nil
+	if ok {
+		sess.Statements = nil
+	}
 }
