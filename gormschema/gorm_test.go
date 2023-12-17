ad4df9f1db3e420e75091edb5b119875936a49e5
dorav <dor.a2003@gmail.com>
2023-12-17 20:07:09 +0200

    moved tests to strict comparison

--- gormschema/gorm_test.go
+++ gormschema/gorm_test.go
@@ -1,7 +1,7 @@
 package gormschema
 
 import (
-	"strings"
+	"os"
 	"testing"
 
 	"ariga.io/atlas-go-sdk/recordriver"
@@ -12,71 +12,44 @@ import (
 )
 
 func TestSQLiteConfig(t *testing.T) {
-	resetSession(t)
+	resetSession()
 	l := New("sqlite")
 	sql, err := l.Load(models.Pet{}, models.User{}, ckmodels.Event{}, ckmodels.Location{})
 	require.NoError(t, err)
-	stmts := strings.Split(sql, "\n")
-	require.Contains(t, stmts[0], "CREATE TABLE `users`")
-	require.Contains(t, stmts[2], "CREATE TABLE `pets`")
-	require.Contains(t, stmts[2], "CONSTRAINT `fk_users_pets` FOREIGN KEY (`user_id`)")
-	require.Contains(t, stmts[4], "CREATE TABLE `locations`")
-	require.Contains(t, stmts[4], "CONSTRAINT `fk_events_location` FOREIGN KEY (`eventId`)")
-	require.Contains(t, stmts[6], "CREATE TABLE `events`")
-	require.Contains(t, stmts[6], "CONSTRAINT `fk_locations_event` FOREIGN KEY (`locationId`)")
-	resetSession(t)
+	requireEqualContent(t, sql, "testdata/sqlite_default")
+	resetSession()
 	l = New("sqlite", WithConfig(&gorm.Config{
 		DisableForeignKeyConstraintWhenMigrating: true,
 	}))
 	sql, err = l.Load(models.Pet{}, models.User{})
 	require.NoError(t, err)
-	stmts = strings.Split(sql, "\n")
-	require.Contains(t, stmts[0], "CREATE TABLE `users`")
-	require.Contains(t, stmts[2], "CREATE TABLE `pets`")
-	require.NotContains(t, stmts[2], "FOREIGN KEY")
-	resetSession(t)
+	requireEqualContent(t, sql, "testdata/sqlite_no_fk")
+	resetSession()
 }
 
 func TestPostgreSQLConfig(t *testing.T) {
-	resetSession(t)
+	resetSession()
 	l := New("postgres")
 	sql, err := l.Load(ckmodels.Location{}, ckmodels.Event{}, models.User{}, models.Pet{})
 	require.NoError(t, err)
-	stmts := strings.Split(sql, "\n")
-	require.Contains(t, stmts[0], `CREATE TABLE "events"`)
-	require.Contains(t, stmts[2], `CREATE TABLE "locations"`)
-	require.Contains(t, stmts[4], `CREATE TABLE "users"`)
-	require.Contains(t, stmts[6], `CREATE TABLE "pets"`)
-	require.Contains(t, stmts[8], `ALTER TABLE "events" ADD CONSTRAINT "fk_locations_event" FOREIGN KEY ("locationId")`)
-	require.Contains(t, stmts[9], `ALTER TABLE "locations" ADD CONSTRAINT "fk_events_location"`)
-	require.Contains(t, stmts[10], `ALTER TABLE "pets" ADD CONSTRAINT "fk_users_pets" FOREIGN KEY ("user_id")`)
-	resetSession(t)
+	requireEqualContent(t, sql, "testdata/postgresql_default")
+	resetSession()
 	l = New("postgres", WithConfig(
 		&gorm.Config{
 			DisableForeignKeyConstraintWhenMigrating: true,
 		}))
 	sql, err = l.Load(ckmodels.Location{}, ckmodels.Event{})
 	require.NoError(t, err)
-	stmts = strings.Split(sql, "\n")
-	require.Contains(t, stmts[0], `CREATE TABLE "events"`)
-	require.Contains(t, stmts[2], `CREATE TABLE "locations"`)
-	require.NotContains(t, sql, "FOREIGN KEY")
+	requireEqualContent(t, sql, "testdata/postgresql_no_fk")
 }
 
 func TestMySQLConfig(t *testing.T) {
-	resetSession(t)
+	resetSession()
 	l := New("mysql")
 	sql, err := l.Load(ckmodels.Location{}, ckmodels.Event{}, models.User{}, models.Pet{})
-	stmts := strings.Split(sql, "\n")
 	require.NoError(t, err)
-	require.Contains(t, stmts[0], "CREATE TABLE `events`")
-	require.Contains(t, stmts[1], "CREATE TABLE `locations`")
-	require.Contains(t, stmts[2], "CREATE TABLE `users`")
-	require.Contains(t, stmts[3], "CREATE TABLE `pets`")
-	require.Contains(t, stmts[4], "ALTER TABLE `events` ADD CONSTRAINT `fk_locations_event`")
-	require.Contains(t, stmts[5], "ALTER TABLE `locations` ADD CONSTRAINT `fk_events_location`")
-	require.Contains(t, stmts[6], "ALTER TABLE `pets` ADD CONSTRAINT `fk_users_pets`")
-	resetSession(t)
+	requireEqualContent(t, sql, "testdata/mysql_default")
+	resetSession()
 	l = New("mysql", WithConfig(
 		&gorm.Config{
 			DisableForeignKeyConstraintWhenMigrating: true,
@@ -84,15 +57,18 @@ func TestMySQLConfig(t *testing.T) {
 	))
 	sql, err = l.Load(ckmodels.Location{}, ckmodels.Event{})
 	require.NoError(t, err)
-	stmts = strings.Split(sql, "\n")
-	require.Contains(t, stmts[0], "CREATE TABLE `events`")
-	require.Contains(t, stmts[1], "CREATE TABLE `locations`")
-	require.NotContains(t, sql, "FOREIGN KEY")
+	requireEqualContent(t, sql, "testdata/mysql_no_fk")
 }
 
-func resetSession(t *testing.T) {
+func resetSession() {
 	sess, ok := recordriver.Session("gorm")
 	if ok {
 		sess.Statements = nil
 	}
+}
+
+func requireEqualContent(t *testing.T, expected, fileName string) {
+	buf, err := os.ReadFile(fileName)
+	require.NoError(t, err)
+	require.Equal(t, expected, string(buf))
 }
