66b75de87a6e061b380360418efaa7b9f711f052
Tran Minh Luan <tranminhluan1206@gmail.com>
2024-07-11 16:44:20 +0700

    gormschema: supports trigger (#50)
    * refactor: re-order code blocks: public first
    
    * feat: allow defining Triggers() for a model
    
    * fix: use Triggers return type as an array of Options
    
    * tests: update test cases for trigger
    
    * docs: update usage for trigger
    
    * refactor: change creating trigger API
    
    * chore: re-hash atlas.sum
    
    * fix: remove unicode character
    
    * fix: correct sqlite migration files
    
    * chore: add comments
    
    * docs: change trigger usage

---
+++ internal/testdata/migrations/sqlite/20240528162330.sql
@@ -0,0 +1,16 @@
+-- Create trigger "trg_insert_user_pet_history"
+CREATE TRIGGER `trg_insert_user_pet_history` AFTER INSERT ON `pets` FOR EACH ROW BEGIN
+	INSERT INTO user_pet_histories (user_id, pet_id, created_at)
+	VALUES (NEW.user_id, NEW.id, datetime('now'));
+END;
+-- Create trigger "trg_adding_heart_on_pet"
+CREATE TRIGGER `trg_adding_heart_on_pet` BEFORE INSERT ON `pets` FOR EACH ROW BEGIN
+	UPDATE pets SET name = name || ' <3' WHERE id = NEW.id;
+END;
+-- Create "user_pet_histories" table
+CREATE TABLE `user_pet_histories` (
+  `user_id` integer NULL,
+  `pet_id` integer NULL,
+  `created_at` datetime NULL,
+  PRIMARY KEY (`user_id`, `pet_id`)
+);
